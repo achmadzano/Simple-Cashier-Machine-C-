10c2b2ea05c4364c2077f6da7415d004080e5b05
Achmad Zano <88878052+achmadzano@users.noreply.github.com>
2021-08-13 18:53:35 +0700

    Update mesin_kasir.cpp

--- mesin_kasir.cpp
+++ mesin_kasir.cpp
@@ -3,29 +3,44 @@
 #include <stdlib.h>
 
 struct Discount{
+    char kode[255];
+    double persen;
+};
+
+double hasildiskon(char target[255], double harga){
+    /*
+        IMPORTANT:
+        1. gudang.txt with discount codes file must be saved in the same directory
+        2. code format : code#percentage
+        3. struct Discount{
                 char kode[255];
                 double persen;
             };
-
-double hasildiskon(char target[255], double harga){
-
+            Above structure must be created.
+            
+        How it works:
+        1. This function checks if user's input 'target' exists in gudang.txt
+        2. If yes:
+            A. fscanf checks for paired discount percentage
+            B. returns discounted price
+        3. If no:
+            A. returns undiscounted price
+    */
     Discount discount[100];
     FILE *fp = fopen("gudang.txt", "r");
     int counter = 0;
     double hasil = 0.0;
-    while(fscanf(fp, "%[^#]#%lf\n", discount[counter].kode,
-          &discount[counter].persen) != EOF){
+    while(fscanf(fp, "%[^#]#%lf\n", discount[counter].kode, &discount[counter].persen) != EOF){
         counter++;
     }
     for(int i=0; i<counter;i++){
         if (strcmp(target,discount[i].kode) == 0){
             hasil = harga - harga * discount[i].persen;
             return hasil;
-        }else{
-            return harga;
         }
     }
     fclose(fp);
+    return harga;
 }
 
 int main() {
@@ -49,23 +64,25 @@ int main() {
         case '1':
             printf("--------------------------------\n");
             printf("Total yang harus dibayar: Rp. ");
-    scanf("%lf", &harga);
+    		scanf("%lf", &harga);
 
-    printf("Masukkan Kode Diskon: ");
-    total_setelah_diskon = hasildiskon(target, harga);
-     scanf("%s", target);
-
+   			printf("Masukkan Kode Diskon: ");
+   			scanf("%s", target);
+   			
+    		total_setelah_diskon = hasildiskon(target, harga);
+     		
+     		printf("Total Setelah Diskon: %.3lf\n", total_setelah_diskon);
 
 
     printf("Jumlah uang yang diberikan : Rp. ");
     scanf("%lf", &uang);
 
-    if(uang>harga) {
+    if(uang>total_setelah_diskon) {
         kembalian = uang - total_setelah_diskon;
         printf("Uang kembalian : Rp. %.3lf", kembalian);
 
     }
-    else if(uang<harga) {
+    else if(uang<total_setelah_diskon) {
         printf("Uang kurang\n");
     }
     else {
@@ -74,25 +91,5 @@ int main() {
 
 
             break;
-
-        case '2':
-            printf("--------------------------------\n");
-            printf("Selamat tinggal!");
-
-
-            break;
-        default:
-            printf("--------------------------------\n");
-            printf("Nomor yang anda masukkan salah");
-
-
-    }
-
-
-
-
-
-
-
-    return 0;
+	}	
 }
